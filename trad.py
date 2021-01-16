6042ac375f9d48aaeff668f85a6e30faa95907ca
Mike3285 <m.romanin94@gmail.com>
2021-01-16 22:43:15 +0100

    script updated

--- trad.py
+++ trad.py
@@ -3,17 +3,19 @@ This file is a script I'm trying to make to make my work a bit faster. Still wor
 Produces a new file from which you can copy and append to the translation file you want to improve
 """
 import pprint
+
 pp = pprint.PrettyPrinter(indent=4)
+
 
 def make_translation_dict(path):
     file = open(path, 'r+', encoding='utf8')
     lines = [line.strip() for line in file.readlines()]
-    finaldict = {line.split('=')[0].strip(' ') : line.split('=')[1].strip(' ') for line in lines[1:] if '= 'in line}
+    finaldict = {line.split('=')[0].strip(' '): line.split('=')[1].strip(' ') for line in lines[1:] if '= ' in line}
     return finaldict
 
 
 def subtraction(t1, t2):
-    finaldict = {k : "" for k in t1 if not k in t2}
+    finaldict = {k: "" for k in t1 if not k in t2}
     return finaldict
 
 
@@ -33,7 +35,7 @@ def translate(m, e):
                     break
                 elif r.lower() == "n":
                     pass
-            print(f'Tradurre "{eng.replace(",","")}" con "{i}" ({key})?')
+            print(f'Tradurre "{eng.replace(",", "")}" con "{i}" ({key})?')
             r = input()
             if r.lower() == "":
                 n[key] = i
@@ -57,14 +59,14 @@ def translate(m, e):
 
 
 if __name__ == '__main__':
-    translated_file = 'IT/ItemName_IT.txt'
+    file_to_translate = 'IT/ItemName_IT.txt'
     english_file = 'EN/ItemName_EN.txt'
-    translated = make_translation_dict(translated_file)
+    translated = make_translation_dict(file_to_translate)
     english = make_translation_dict(english_file)
     missing = subtraction(english, translated)
     new = translate(missing, english)
     pp.pprint(new)
-    file =  open('new_for_'+translated_file.split('/')[1], 'w+')
+    file = open('new_for_' + file_to_translate.split('/')[1], 'w+')
     for k in new:
         file.write(f'{k} = "{new[k]}",\n')
     file.close()
